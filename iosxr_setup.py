d7585bfd432fa8a975f007d3a791757d20ad5f15
Richard Wellum <rwellum@cisco.com>
2016-06-30 17:29:31 -0400

    ssh inconsistencies and global-vrf
    SSH was failing due to the run - then a block of commands. Swapped to
    using bash -c - but wrapped in a new fn() which also waits for the
    prompt.
    
    Swapped from run to bash wherever possible so user is in global-vrf

--- iosxr_setup.py
+++ iosxr_setup.py
@@ -36,6 +36,25 @@ class XrLogin(object):
         self.xr_telnet_sessions.append(self.xr1)
 
     #
+    # Send a global-vrf XR Linux command - wait for prompt
+    # Could do bash -c or run ip netns exec global-vrf bash
+    #
+    def send_operns(self, command):
+        xr1 = self.xr1
+
+        xr1.send("bash -c %s" % command)
+        xr1.wait("[\$#]")
+
+    #
+    # Send a xrnns XR Linux command - wait for prompt
+    #
+    def send_xrnns(self, command):
+        xr1 = self.xr1
+
+        xr1.send("run %s" % command)
+        xr1.wait("[\$#]")
+
+    #
     # Configure a vagrant box
     #
     def setup_networking(self, host_ip=None, gateway=None, allow_root_login=False):
@@ -54,6 +73,8 @@ class XrLogin(object):
         time.sleep(2)
         output = xr1.wait("[\$#]")
         k9 = re.search(r'-k9sec', output)
+        if k9:
+            xr1.log("Crypto k9 image detected")
 
         # Wait for a management interface to be available
         xr1.repeat_until("sh run | inc MgmtEth",
@@ -115,50 +136,48 @@ class XrLogin(object):
         if allow_root_login:
             xr1.send("bash -c sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config_operns")
 
-        #
-        # Send commands to XR Linux
-        #
-        xr1.send("run")
-
         # Add passwordless sudo as required by jenkins
-        xr1.send("echo '####Added by iosxr_setup to give vagrant passwordless access' >> /etc/sudoers")
-        xr1.send("echo '%sudo ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers")
+        self.send_operns("echo '####Added by iosxr_setup to give vagrant passwordless access' >> /etc/sudoers")
+        self.send_operns("echo '%sudo ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers")
+        self.send_operns("echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers")
 
         # Add public key, so users can ssh without a password
         # https://github.com/purpleidea/vagrant-builder/blob/master/v6/files/ssh.sh
-        xr1.send("[ -d ~vagrant/.ssh ] || mkdir ~vagrant/.ssh")
-        xr1.send("chmod 0700 ~vagrant/.ssh")
-        xr1.send("echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > ~vagrant/.ssh/authorized_keys")
-        xr1.send("chmod 0600 ~vagrant/.ssh/authorized_keys")
-        xr1.send("chown -R vagrant:vagrant ~vagrant/.ssh/")
+        self.send_operns("[ -d ~vagrant/.ssh ] || mkdir ~vagrant/.ssh")
+        self.send_operns("chmod 0700 ~vagrant/.ssh")
+        self.send_operns("echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > ~vagrant/.ssh/authorized_keys")
+        self.send_operns("chmod 0600 ~vagrant/.ssh/authorized_keys")
+        self.send_operns("chown -R vagrant:vagrant ~vagrant/.ssh/")
 
         # Add user scratch space - should be able transfer a file to
         # /misc/app_host/scratch using the scp with user vagrant
         # E.g: scp -P 2200 Vagrantfile vagrant@localhost:/misc/app_host/scratch
-        xr1.send("groupadd app_host")
-        xr1.send("usermod -a -G app_host vagrant")
-        xr1.send("mkdir -p /misc/app_host/scratch")
-        xr1.send("chgrp -R app_host /misc/app_host/scratch")
-        xr1.send("chmod 777 /misc/app_host/scratch")
+        self.send_operns("groupadd app_host")
+        self.send_operns("usermod -a -G app_host vagrant")
+        self.send_operns("mkdir -p /misc/app_host/scratch")
+        self.send_operns("chgrp -R app_host /misc/app_host/scratch")
+        self.send_operns("chmod 777 /misc/app_host/scratch")
 
         # Add Cisco OpenDNS IPv4 nameservers as a default DNS resolver
         # almost all users who have internet connectivity will be able to reach those.
         # This will prevent users from needing to supply another Vagrantfile or editing /etc/resolv.conf manually
-        xr1.send("echo '# Cisco OpenDNS IPv4 nameservers' >> /etc/resolv.conf")
-        xr1.send("echo 'nameserver 208.67.222.222' >> /etc/resolv.conf")
-        xr1.send("echo 'nameserver 208.67.220.220' >> /etc/resolv.conf")
+        # Doing this in xrnns because the syncing of cat /etc/netns/global-vrf/resolv.conf to
+        # /etc/resolv.conf requires 'ip netns exec global-vrf bash'.
+        xr1.send("run echo '# Cisco OpenDNS IPv4 nameservers' > /etc/resolv.conf")
+        xr1.send("run echo 'nameserver 208.67.222.222' >> /etc/resolv.conf")
+        xr1.send("run echo 'nameserver 208.67.220.220' >> /etc/resolv.conf")
 
         # Start operns sshd server so vagrant ssh can access app-hosting space
-        xr1.send("service sshd_operns start")
+        self.send_operns("service sshd_operns start")
 
         # Wait for it to come up
-        xr1.repeat_until("service sshd_operns status",
+        xr1.repeat_until("bash -c service sshd_operns status",
                          match_txt="is running...",
                          debug="Check sshd_operns is up",
                          timeout=5)
 
-        xr1.send("chkconfig --add sshd_operns")
-        xr1.send("exit")
+        self.send_operns("chkconfig --add sshd_operns")
+
         xr1.wait("RP/0/RP0/CPU0:ios")
 
         # Set up IOS XR ssh if a k9/crypto image
@@ -166,7 +185,6 @@ class XrLogin(object):
             xr1.send("crypto key generate rsa")
             xr1.wait("How many bits in the modulus")
             xr1.send("")  # Send enter to get default 2048
-            xr1.wait("[\$#]")  # Wait for the prompt
 
     def get_mgmt_ip(self):
         xr1 = self.xr1
